d298231f7017a38222034982287e444f809b6b81
HongCaiJin <msjkgoodluck@gmail.com>
2018-10-17 10:58:00 +0800

    add message js

---
+++ src/firebase-messaging-sw.js
@@ -0,0 +1,9 @@
+importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-messaging.js');
+
+
+firebase.initializeApp({
+    'messagingSenderId': '697564753721'
+});
+
+const messaging = firebase.messaging();
