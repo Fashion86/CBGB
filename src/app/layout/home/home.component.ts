395752e437d01aa90daad6b90097d6f0ed9db4eb
HongCaiJin <msjkgoodluck@gmail.com>
2018-10-29 10:07:06 +0800

    email send on contact

--- src/app/layout/home/home.component.ts
+++ src/app/layout/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import * as emailjs from 'emailjs-com';
+import {ToastrService} from 'ngx-toastr';
+import {NgxSpinnerService} from 'ngx-spinner';
+import { Message } from '../../model/message';
 
 @Component({
   selector: 'app-home',
@@ -8,11 +12,31 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   imageUrlArray: any[] = [];
-  constructor() { }
+  msg: Message;
+  constructor(private alerts: ToastrService,
+              private spinner: NgxSpinnerService) { }
 
   ngOnInit() {
+    this.msg = new Message();
     this.imageUrlArray.push('assets/img/slide.jpg');
     this.imageUrlArray.push('assets/img/slide-m.jpg');
+  }
+  onsendMail() {
+    this.spinner.show();
+    const messageBody  = {
+      'reply_to': this.msg.email,
+      'from_name': this.msg.name,
+      'to_name': 'CBGB',
+      'message_html': this.msg.message
+    };
+    emailjs.send('gmail', 'template_hc86m4E3', messageBody, 'user_L4WqeuVWonx60Ytaxv6jS')
+      .then((response) => {
+        this.spinner.hide();
+        this.alerts.success( 'Mensaje éxito');
+      }, (err) => {
+        this.spinner.hide();
+        this.alerts.error('FAILED...', err);
+      });
   }
 
 }
