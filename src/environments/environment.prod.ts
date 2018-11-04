c10476dc0c3c2d1fcf482e386536cb5ee5ce97f6
msjkg <msjkgoodluck@gmail.com>
2018-11-04 22:31:50 +0800

    remove key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
-  production: true,
-  firebase: {
-    apiKey: "AIzaSyBDcCcOCRbxCncLWkv-ZU8V4B8a3zlqE-I",
-    authDomain: "cbgb-pwa-2018.firebaseapp.com",
-    databaseURL: "https://cbgb-pwa-2018.firebaseio.com",
-    projectId: "cbgb-pwa-2018",
-    storageBucket: "cbgb-pwa-2018.appspot.com",
-    messagingSenderId: "276232773286"
-  }
+  // production: true,
+  // firebase: {
+  //   apiKey: "AIzaSyBDcCcOCRbxCncLWkv-ZU8V4B8a3zlqE-I",
+  //   authDomain: "cbgb-pwa-2018.firebaseapp.com",
+  //   databaseURL: "https://cbgb-pwa-2018.firebaseio.com",
+  //   projectId: "cbgb-pwa-2018",
+  //   storageBucket: "cbgb-pwa-2018.appspot.com",
+  //   messagingSenderId: "276232773286"
+  // }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,15 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  firebase: {
-    apiKey: "AIzaSyBDcCcOCRbxCncLWkv-ZU8V4B8a3zlqE-I",
-    authDomain: "cbgb-pwa-2018.firebaseapp.com",
-    databaseURL: "https://cbgb-pwa-2018.firebaseio.com",
-    projectId: "cbgb-pwa-2018",
-    storageBucket: "cbgb-pwa-2018.appspot.com",
-    messagingSenderId: "276232773286"
-  }
+  // production: false,
+  // firebase: {
+  //   apiKey: "AIzaSyBDcCcOCRbxCncLWkv-ZU8V4B8a3zlqE-I",
+  //   authDomain: "cbgb-pwa-2018.firebaseapp.com",
+  //   databaseURL: "https://cbgb-pwa-2018.firebaseio.com",
+  //   projectId: "cbgb-pwa-2018",
+  //   storageBucket: "cbgb-pwa-2018.appspot.com",
+  //   messagingSenderId: "276232773286"
+  // }
 };
 /*
  * For easier debugging in development mode, you can import the following file
